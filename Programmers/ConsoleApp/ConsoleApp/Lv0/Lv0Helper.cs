4141184bda2a364651f9ea497b19408669a0382f
minmaxheap <minmaxheap@naver.com>
2024-04-02 15:48:45 +0900

    프로그래머스 lv0 생성
    [이민희 2024.04.02]
    1. 프로그래머스 lv0 생성

---
+++ Programmers/ConsoleApp/ConsoleApp/Lv0/Lv0Helper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Lv0
+{
+    internal class Lv0Helper
+    {
+        internal void Lv0Method()
+        {
+            try
+            {
+                #region 문자열 출력하기
+                Console.WriteLine("문자열 출력하기");
+
+                #endregion
+
+                #region a와 b 출력하기
+                Console.WriteLine("a와 b 출력하기");
+                #endregion
+
+
+                #region 문자열 반복해서 출력하기
+                Console.WriteLine("문자열 반복해서 출력하기");
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
