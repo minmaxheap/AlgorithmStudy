e05511f920f190f57d32f4018cba292694aebf75
VMSmhlee <minmaxheap@naver.com>
2024-04-05 17:38:22 +0900

    백준 폴더 생성
    [이민희 2024.04.05]
    1. 백준 폴더 생성

---
+++ Baekjoon/ConsoleApp/ConsoleApp/Lv0/Lv0Helper.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Lv0
+{
+    internal class Lv0Helper
+    {
+        internal void Lv0Method()
+        {
+            try
+            {
+                //Lv0Method1();
+                //Lv0Method2();
+                Lv0Method3();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        #region 문자열 출력하기
+
+        /// <summary>
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/181952
+        /// </summary>
+        internal void Lv0Method1()
+        {
+            try
+            {
+                string s;
+
+                Console.Clear();
+                s = Console.ReadLine();
+                Console.WriteLine(s);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        #endregion
+        #region a와 b 출력하기
+        /// <summary>
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/181951
+        /// </summary>
+        internal void Lv0Method2()
+        {
+            try
+            {
+                string[] s;
+                Console.Clear();
+                s = Console.ReadLine().Split(' ');
+                int a = int.Parse(s[0]);
+                int b = int.Parse(s[1]);
+                Console.WriteLine($"a = {a}");
+                Console.WriteLine($"b = {b}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        #endregion
+        #region 문자열 반복해서 출력하기
+        /// <summary>
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/181950
+        /// </summary>
+        internal void Lv0Method3()
+        {
+            try
+            {
+                string[] strArr;
+                Console.Clear();
+                strArr = Console.ReadLine().Split(' ');
+                int a = int.Parse(strArr[1]);
+                for(int i = 0; i < a; i++)
+                {
+                    Console.Write(strArr[0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Baekjoon/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp.Lv0;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lv0Helper lv0Helper = new Lv0Helper();
+            lv0Helper.Lv0Method();
+        }
+    }
+}
