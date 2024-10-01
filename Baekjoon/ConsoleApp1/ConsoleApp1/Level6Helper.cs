558f9bb0cf7d3fdadf302e3de7aef51b3ca46c09
minmaxheap <minmaxheap@naver.com>
2024-10-01 17:28:32 +0900

    심화 1 - 킹, 퀸, 룩, 비숍, 나이트, 폰
    심화 1 - 킹, 퀸, 룩, 비숍, 나이트, 폰

--- Baekjoon/ConsoleApp1/ConsoleApp1/Level6Helper.cs
+++ Baekjoon/ConsoleApp1/ConsoleApp1/Level6Helper.cs
@@ -50,5 +50,35 @@ namespace ConsoleApp1
             }
         }
         #endregion
+        #region 킹, 퀸, 룩, 비숍, 나이트, 폰
+        internal void Level6Method02()
+        {
+            try
+            {
+                //입력값 배열로 변환
+                string? str = Console.ReadLine();
+                if (string.IsNullOrEmpty(str)) return;
+                string[] inputs = str.Split(' ');
+
+                //배열 초기화
+                int[] arr = new int[6] { 1, 1, 2, 2, 2, 8 };
+                int[] result = new int[6];
+                for (int i = 0; i < inputs.Length; i++)
+                {
+                    int x = Convert.ToInt32(inputs[i]);
+                    //arr[i]-inputs[i]
+                    int temp = arr[i] - x;
+                    result[i] = temp;
+                }
+                //배열 공백으로 연결하여 출력
+                Console.WriteLine(string.Join(' ', result));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        #endregion
     }
 }
